62d273bcca0c30462486990a34f2fefc2b0ebccb
Robert Usher <266585+dcchut@users.noreply.github.com>
2021-12-02 16:18:39 +1100

    Q2 solution

--- src/questions/q2.rs
+++ src/questions/q2.rs
@@ -1,13 +1,73 @@
-use crate::{ProblemInput, Solution};
+use crate::{FromProblemInputLine, ProblemInput, Solution};
+use std::str::FromStr;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Direction {
+    Forward,
+    Up,
+    Down,
+}
+
+impl FromStr for Direction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "up" => Ok(Direction::Up),
+            "down" => Ok(Direction::Down),
+            "forward" => Ok(Direction::Forward),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Movement {
+    direction: Direction,
+    quantity: i64,
+}
+
+impl FromProblemInputLine for Movement {
+    fn from_line(line: &str) -> Self {
+        let parts: Vec<_> = line.split(' ').collect();
+        let direction = Direction::from_str(parts[0]).unwrap();
+        let quantity = parts[1].parse().unwrap();
+
+        Movement {
+            direction,
+            quantity,
+        }
+    }
+}
 
 pub struct Q2;
 
 impl Solution for Q2 {
-    fn part1(&self, _lines: &ProblemInput) -> String {
-        String::new()
+    fn part1(&self, lines: &ProblemInput) -> String {
+        let movements: Vec<Movement> = lines.parse();
+
+        let (h, d) = movements
+            .into_iter()
+            .fold((0, 0), |(h, d), m| match m.direction {
+                Direction::Up => (h, d - m.quantity),
+                Direction::Down => (h, d + m.quantity),
+                Direction::Forward => (h + m.quantity, d),
+            });
+
+        (h * d).to_string()
     }
 
-    fn part2(&self, _lines: &ProblemInput) -> String {
-        String::new()
+    fn part2(&self, lines: &ProblemInput) -> String {
+        let movements: Vec<Movement> = lines.parse();
+
+        let (h, d, _) = movements
+            .into_iter()
+            .fold((0, 0, 0), |(h, d, aim), m| match m.direction {
+                Direction::Up => (h, d, aim - m.quantity),
+                Direction::Down => (h, d, aim + m.quantity),
+                Direction::Forward => (h + m.quantity, d + aim * m.quantity, aim),
+            });
+
+        (h * d).to_string()
     }
 }
